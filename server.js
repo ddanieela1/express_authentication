da5c2ce7953c7b85fb077fcf967ae7c600d32a41
ddanieela1 <vdaniela.gv@gmail.com>
2022-09-10 15:58:14 -0400

    add: user model and validations

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use(layouts);
+app.use(layouts);//ejs layouts
 
 app.get('/', (req, res) => {
   res.render('index');
