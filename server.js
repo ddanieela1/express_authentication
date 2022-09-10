621e8f4ebaafe369de6fbca4d418633553e9f55b
ddanieela1 <vdaniela.gv@gmail.com>
2022-09-10 18:48:15 -0400

    server: Import passport and pass through middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
 const { response } = require('express');
+const passport = require('./config/ppConfig');
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
 console.log('hi',SECRET_SESSION);
@@ -24,6 +25,8 @@ app.use(session({
 }));
 
 app.use(flash());            // flash middleware
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
 
 app.use((req,res,next)=>{
   console.log('res locals >>>', res.locals) //.local is an object inside of the .res, key value of res object
