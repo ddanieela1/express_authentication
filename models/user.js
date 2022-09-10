4425af84d4fe235e85b03e7f15ebadaba2996fc4
ddanieela1 <vdaniela.gv@gmail.com>
2022-09-10 17:52:49 -0400

    chore: add bcrypt and functions to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 const {
   Model
 } = require('sequelize');
@@ -45,5 +47,27 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'user',
   });
 
+  //line 50-56 will run function once when user is creating accouunt
+// Before a user is created, we are encrypting the password and using hash in its place
+user.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
+  // Bcrypt is going to hash the password
+  let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times,when password is inputted it, it will be hashed 12 times,
+  //to prevent hacking account and will return a string of characters
+  pendingUser.password = hash; // this will go to the DB
+});  
+
+// Check the password on Sign-In and compare it to the hashed password in the DB
+user.prototype.validPassword = function(typedPassword) {
+  let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+
+  return isCorrectPassword;
+}
+// return an object from the database of the user without the encrypted password
+user.prototype.toJSON = function() {
+  let userData = this.get(); 
+  delete userData.password; // it doesn't delete password from database, only removes it to the view user
+  
+  return userData;
+}
   return user; // add functions above 
 };
